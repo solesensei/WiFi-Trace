1aebd4a1527415b57445dd934b17d3cf8677747f
SoleSensei <dimargfd@gmail.com>
2017-11-24 03:18:49 +0300

    glm library replaced

---
+++ include/glm/CMakeFiles/glm.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/glm.dir/core/dummy.o"
+  "glm.pdb"
+  "glm"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/glm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
