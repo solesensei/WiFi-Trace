fecc0cd0805d5f0e2c4e3e9f76c9a98a4821ebbd
SoleSensei <dimargfd@gmail.com>
2017-11-28 11:25:30 +0300

    glm library added

---
+++ externals/glm/CMakeFiles/glm_dummy.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/glm_dummy.dir/detail/glm.o"
+  "CMakeFiles/glm_dummy.dir/detail/dummy.o"
+  "glm_dummy.pdb"
+  "glm_dummy"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/glm_dummy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
