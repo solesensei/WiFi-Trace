3db402949e00bf86d249908a95ec77b440781c68
SoleSensei <dimargfd@gmail.com>
2017-12-03 14:33:42 +0300

    test script update

--- test.sh
+++ test.sh
@@ -2,15 +2,21 @@
 
 if [ $# -eq 1 ] # if args
 then
-    # Remake project
-    sudo make clean
-    sudo make
-    cd build/bin
-    # Launch project with config file parameters
-    echo 
-    echo "Test config"
-    echo CONFIG: config_file$1.cfg
-    sudo ./visualize --config_file config_file$1.cfg
+    if [ $1 -ge 1 ] && [ $1 -le 3 ]
+    then
+        # Remake project
+        sudo make clean
+        sudo make
+        cd build/bin
+        # Launch project with config file parameters
+        echo 
+        echo "Test config"
+        echo CONFIG: config_file$1.cfg
+        sudo ./visualize --config_file config_file$1.cfg
+    else
+        echo no such config file [config_file$1.cfg]. Aborted.
+        echo choose \'1-3\' configs, or use default parameters    
+    fi
 else # no args
     # Make project
     sudo make
